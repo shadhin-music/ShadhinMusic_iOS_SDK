538d04b8b3876a92e5697442647d6f39a9fd3e09
Md Maruf prodhan <59418855+mdmarufprodhan@users.noreply.github.com>
2026-03-16 12:05:42 +0600

    Release v1.0.7

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         )
     ],
     targets: [
-        // Wrapper source target — required for Xcode SPM compatibility
         .target(
             name: "ShadhinGP",
             dependencies: [
@@ -27,47 +26,13 @@ let package = Package(
             ],
             path: "Sources/ShadhinGP"
         ),
-
-        // Binary targets
-        .binaryTarget(
-            name: "ShadhinGPBinary",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/ShadhinGP.xcframework.zip",
-            checksum: "e82d3eda76f184a139900d2dc21ca37ed937aee8063af1b4c52c7da1013b484e"
-        ),
-        .binaryTarget(
-            name: "Vmax",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/Vmax.xcframework.zip",
-            checksum: "dd27ff70ef6fe90ad5905bb3e98c7d95dca2233c332d48ffc2b489ddf2bad9a5"
-        ),
-        .binaryTarget(
-            name: "VmaxVastHelper",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/VmaxVastHelper.xcframework.zip",
-            checksum: "b8f6416fcf6aa90166202d8fcce8516ab3c92cec72f283fe4e05c94ba4d0610f"
-        ),
-        .binaryTarget(
-            name: "VmaxVideoHelper",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/VmaxVideoHelper.xcframework.zip",
-            checksum: "d40a2b9a57e5dcbfccc31e6fd4985bf7fa6b819c275b4732dc8c7d49d726c84a"
-        ),
-        .binaryTarget(
-            name: "VmaxNativeHelper",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/VmaxNativeHelper.xcframework.zip",
-            checksum: "563e79bc73eb2402eaa786b1e88d8e9dfe6bae8a39ca802883b4e85199499e51"
-        ),
-        .binaryTarget(
-            name: "VmaxOM",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/VmaxOM.xcframework.zip",
-            checksum: "45dc4ed708aa59d66306d7ccc33f3065f6c5df8265c6b712776683c95f68219f"
-        ),
-        .binaryTarget(
-            name: "VmaxDisplayHelper",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/VmaxDisplayHelper.xcframework.zip",
-            checksum: "41be76f1fd2f2eb44738228f7c4ede1f2b30eddb89b043c949a34bfcb286a865"
-        ),
-        .binaryTarget(
-            name: "OMSDK_Vmax2",
-            url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.4/OMSDK_Vmax2.xcframework.zip",
-            checksum: "90b519d79b13c385779b2f6482e45dfa13e767d3edce1383483c247a462700a2"
-        ),
+        .binaryTarget(name: "ShadhinGPBinary", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/ShadhinGP.xcframework.zip", checksum: "270d33d80655fcf00ec7cea1f28e6375cb4d338bf57ed34aa6df5ab7a9011fe7"),
+        .binaryTarget(name: "Vmax", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/Vmax.xcframework.zip", checksum: "f4d5f194e61b8b0aec50684f08c3fd7e8f25e9b13ef9ea228b00b635d7deb528"),
+        .binaryTarget(name: "VmaxVastHelper", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/VmaxVastHelper.xcframework.zip", checksum: "5448e7ac074f572b85d47fa0fd58fbb6a86f5b260f97171e9c20001f9dd92a85"),
+        .binaryTarget(name: "VmaxVideoHelper", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/VmaxVideoHelper.xcframework.zip", checksum: "9916f2234c94ccba7c57c3abbb0c3184d02350e96143f89a8387710a127e1d94"),
+        .binaryTarget(name: "VmaxNativeHelper", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/VmaxNativeHelper.xcframework.zip", checksum: "147f3cc9ce99d5507fbc4ad4be6b475460e16e1ed17ec6e597da62f7496d4d59"),
+        .binaryTarget(name: "VmaxOM", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/VmaxOM.xcframework.zip", checksum: "08800014d5747d8377b81758ee37bdca4b8b5032b7288482d02f3369d18f9324"),
+        .binaryTarget(name: "VmaxDisplayHelper", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/VmaxDisplayHelper.xcframework.zip", checksum: "b04a7dfce4edaba56f9d09e54a90c433dc1f8cfe2b1d3017dd433f16160e3961"),
+        .binaryTarget(name: "OMSDK_Vmax2", url: "https://github.com/shadhin-music/ShadhinMusic_iOS_SDK/releases/download/1.0.7/OMSDK_Vmax2.xcframework.zip", checksum: "205a38fc6360b930c57c016cea11b0deb5ccba0e9116a22f537d6f8e81155b6b"),
     ]
 )
